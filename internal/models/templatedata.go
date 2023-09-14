1888d693fc6b2a42948e3e849bb13d0859e4715f
saroyangor <dr.saroyan98@gmail.com>
2023-09-14 15:23:46 +0400

    Authentication

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -3,13 +3,14 @@ package models
 import "github.com/saroyangor/bookings/internal/forms"
 
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	IntFloat  map[string]float32
-	Data      map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	IntFloat        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
